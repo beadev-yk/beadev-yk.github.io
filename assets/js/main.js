fe5a631b896114cd0a9b3d0cd314d4ab4378e7e8
Yashwant Kushwaha <ykkmec@gmail.com>
2025-04-24 23:59:49 +0530

    Modified experience and Projects data

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,26 @@
 AOS.init();
 
-// You can also pass an optional settings object
-// below listed default settings
 AOS.init({
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-  offset: 120, // offset (in px) from the original trigger point
-  delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 800, // values from 0 to 3000, with step 50ms
-  easing: "ease", // default easing for AOS animations
-  once: false, // whether animation should happen only once - while scrolling down
-  mirror: false, // whether elements should animate out while scrolling past them
-  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+  offset: 120,
+  delay: 0,
+  duration: 800,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  new Typed("#typed-text", {
+    strings: [
+      "FRONT-END DEVELOPER.",
+      "BACK-END DEVELOPER.",
+      "FULL STACK DEVELOPER.",
+      "MENTOR.",
+    ],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1500,
+    loop: true,
+  });
 });
